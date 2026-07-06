6c6a749e16dd29d21da345372e5805af980a64c4
Francowski <taypehuamanifranco@gmail.com>
2026-07-06 09:36:14 -0500

    feat(seller): implement seller layout module

--- src/app/features/seller/seller.ts
+++ src/app/features/seller/seller.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Navbar } from './layout/navbar/navbar';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-seller',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Sidebar,
+    Navbar,
+    ToastModule,
+    ConfirmDialogModule
+  ],
   templateUrl: './seller.html',
   styleUrl: './seller.css',
 })
