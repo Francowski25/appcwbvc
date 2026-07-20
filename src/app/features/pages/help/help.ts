09b4e69540adc284bee0dfa85b7868f3a7ac7f80
Francowski <taypehuamanifranco@gmail.com>
2026-07-20 14:33:46 -0500

    feat(help): update help routes and simplify navbar

--- src/app/features/pages/help/help.ts
+++ src/app/features/pages/help/help.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-help',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './help.html',
   styleUrl: './help.css',
 })
